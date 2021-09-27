5d8c01ae107e35495a2e05ab56eb76749c40e18d
Ethan Marshall <ethan@ethanjmarshall.co.uk>
2021-09-27 10:21:36 +0100

    Add countdown clock support to stopwatch

--- stopwatch/stopwatch.go
+++ stopwatch/stopwatch.go
@@ -1,13 +1,13 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"os/exec"
-	"time"
-	"flag"
-	"strings"
 	"strconv"
+	"strings"
+	"time"
 )
 
 const (
@@ -20,9 +20,9 @@ const (
 
 var (
 	hour, min, sec, msec int64 = 0, 0, 0, 0
-	paused bool = false
+	paused               bool  = false
+	timer                bool  = false
 )
-
 
 func setupTerminal() {
 	// disable input buffering
@@ -31,25 +31,43 @@ func setupTerminal() {
 	exec.Command("stty", "-F", "/dev/tty", "-echo").Run()
 }
 
+func setupDuration(i string, f string) bool {
+	s := strings.Split(i, ":")
+	if len(s) < 4 {
+		fmt.Fprintf(os.Stderr, "stopwatch: not enough flags to -%s: %q\n", f, i)
+		return false
+	}
+
+	hour, _ = strconv.ParseInt(s[0], 10, 32)
+	min, _ = strconv.ParseInt(s[1], 10, 32)
+	sec, _ = strconv.ParseInt(s[2], 10, 32)
+	msec, _ = strconv.ParseInt(s[3], 10, 32)
+
+	return true
+}
+
 func setupFlags() bool {
 	var starttime string = ""
+	var starttimer string = ""
 
 	flag.BoolVar(&paused, "p", false, "Start paused")
 	flag.StringVar(&starttime, "t", "", "Start time")
+	flag.StringVar(&starttimer, "c", "", "Run in timer mode, counting from...")
 
 	flag.Parse()
 
 	if starttime != "" {
-		s := strings.Split(starttime, ":")
-		if len(s) < 4 {
-			fmt.Fprintf(os.Stderr, "stopwatch: not enough flags to -t: %q\n", starttime)
+		if !setupDuration(starttime, "t") {
 			return false
 		}
+	}
 
-		hour, _ = strconv.ParseInt(s[0], 10, 32)
-		min, _  = strconv.ParseInt(s[1], 10, 32)
-		sec, _  = strconv.ParseInt(s[2], 10, 32)
-		msec, _  = strconv.ParseInt(s[3], 10, 32)
+	if starttimer != "" {
+		timer = true
+
+		if !setupDuration(starttimer, "c") {
+			return false
+		}
 	}
 
 	return true
@@ -84,7 +102,7 @@ func inputLoop(c chan int) {
 			c <- clear
 		case ' ':
 			c <- pause
-		case 'h' :
+		case 'h':
 			c <- help
 		case '?':
 			c <- help
@@ -92,6 +110,66 @@ func inputLoop(c chan int) {
 			fmt.Printf("\nUnknown command: %q\n", i)
 
 		}
+	}
+}
+
+func countLoop() {
+	if msec+1 >= 1000 {
+		sec++
+		msec = 0
+	} else {
+		msec++
+	}
+
+	if sec >= 60 {
+		min++
+		sec = 0
+		msec = 0
+
+	}
+
+	if min >= 60 {
+		hour++
+		min = 0
+		sec = 0
+		msec = 0
+	}
+}
+
+func timerLoop() {
+	if msec == 0 && sec == 0 && min == 0 && hour == 0 {
+		fmt.Print("\a\n")
+
+		cols := [2]string{"\x1b[31m", "\x1b[0m"}
+		for i := 0; i < 5; i++ {
+			fmt.Printf("\r%sTimer up!", cols[i % 2])
+			time.Sleep(500 * time.Millisecond)
+		}
+
+		fmt.Println()
+		os.Exit(0)
+	}
+
+	if msec-1 < 0 {
+		sec--
+		msec = 999
+
+	} else {
+		msec--
+	}
+
+	if sec < 0 {
+		min--
+		msec = 999
+		sec = 59
+	}
+
+	if min < 0 {
+		hour--
+		msec = 999
+		sec = 59
+		min = 59
+
 	}
 }
 
@@ -135,15 +213,15 @@ func main() {
 					os.Stdout.Write([]byte("[H[2J")) // Should work for most ANSII terminals
 				case help:
 					fmt.Println(`
-Available actions:
-	h: help menu
-	?: help menu
-	q: quit
-	c: clear/reset timer
-	Control-l/l: clear/reset screen
-	<space>: pause timer
+					Available actions:
+					h: help menu
+					?: help menu
+					q: quit
+					c: clear/reset timer
+					Control-l/l: clear/reset screen
+					<space>: pause timer
 
-Any keybinding can be pressed at any time and will take effect immediately`)
+					Any keybinding can be pressed at any time and will take effect immediately`)
 				default:
 					fmt.Println("\nWARNING: Unknown command")
 				}
@@ -158,27 +236,11 @@ Any keybinding can be pressed at any time and will take effect immediately`)
 		}
 
 		if !paused {
-			if msec+1 >= 1000 {
-				sec++
-				msec = 0
+			if timer {
+				timerLoop()
 			} else {
-				msec++
+				countLoop()
 			}
-
-			if sec >= 60 {
-				min++
-				sec = 0
-				msec = 0
-
-			}
-
-			if min >= 60 {
-				hour++
-				min = 0
-				sec = 0
-				msec = 0
-			}
-
 
 			fmt.Printf("\r%02d:%02d:%02d:%04d", hour, min, sec, msec)
 		}
