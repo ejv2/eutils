50c1d824a92c2bbce59538258f22f8d96cc6d03d
Ethan Marshall <ethan@ethanjmarshall.co.uk>
2021-10-01 15:18:28 +0100

    Add generation code for chars mode

--- typer/chars.go
+++ typer/chars.go
@@ -1,4 +1,37 @@
 package main
 
+import (
+	"math/rand"
+)
+
+const (
+	maxPhrases int = 4
+	maxChars   int = 15
+
+	minPhrases int = 1
+	minChars   int = 5
+)
+
+var (
+	chars [][]rune
+)
+
+func generate(dataset []rune) {
+	nphrases := rand.Intn(maxPhrases) + minPhrases
+
+	chars = make([][]rune, nphrases)
+
+	for i := range chars {
+		nchars   := rand.Intn(maxChars) + minChars
+		chars[i] = make([]rune, nchars)
+
+		for j := range chars[i] {
+			sel := rand.Intn(len(dataset))
+			chars[i][j] = dataset[sel]
+		}
+	}
+}
+
 func RunChars(dataset []rune) {
+	generate(dataset)
 }
