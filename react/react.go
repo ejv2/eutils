46be3dfe01ec2241630bc668d4e283a5232dc612
Ethan Marshall <ethan@ethanjmarshall.co.uk>
2021-10-21 09:19:09 +0100

    Add average measuring support

--- react/react.go
+++ react/react.go
@@ -1,16 +1,30 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"math/rand"
 	"os"
 	"time"
 )
 
+var (
+	count int
+)
+
+func flags() {
+	flag.IntVar(&count, "c", 0, "Take this many samples")
+
+	flag.Parse()
+}
+
 func main() {
-	for {
+	flags()
+
+	res := make([]time.Duration, count)
+	for i := 0; count == 0 || (count > 0 && i < count); i++ {
 		var c [1]byte
-		wait := time.Duration(rand.Int63n(4) + 1) * time.Second
+		wait := time.Duration(rand.Int63n(4)+1) * time.Second
 
 		fmt.Println("Wait for the prompt and press enter...")
 		time.Sleep(wait)
@@ -22,7 +36,27 @@ func main() {
 		diff := time.Now().Sub(now)
 		fmt.Printf("%v\n", diff)
 
-		fmt.Print("Press enter to reset...")
+		if count != 0 {
+			res[i] = diff
+		}
+
+		fmt.Print("Press enter to continue...")
 		os.Stdin.Read(c[:])
 	}
+
+	var total float64
+	var avg float64
+
+	for _, elem := range res {
+		total += float64(elem.Milliseconds())
+		total += float64(elem.Microseconds()) / 1000
+	}
+	avg = float64(total) / float64(count)
+
+	fmt.Printf(`Result summary
+==============
+
+Total trials run: %d
+Average reaction time (ms): %f
+`, count, avg)
 }
