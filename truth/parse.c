721ca445ad262a9ec6bbd5ee78557df4ac02131c
Ethan Marshall <ethan@ethanjmarshall.co.uk>
2021-11-22 14:04:58 +0000

    Code cleanup

---
+++ truth/parse.c
@@ -0,0 +1,133 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include "truth.h"
+
+static const int opermax = 3;
+static int opers = 0;
+static char operands[128] = {0};
+
+void parseparam(char **program, Statement *stat, int pos)
+{
+	char oper = -1;
+
+	puts("START NAMED PARAMETER");
+	while (isalnum(*(*program+1))) {
+		printf("\tPARAMPARSE: %c\n", **program);
+
+		operands[opers++] = **program;
+
+		if (oper == -1) {
+			oper = **program;
+			stat->operands[pos].data.oper = oper;
+		}
+
+		(*program)++;
+	}
+
+	printf("Operand ends on '%c'\n", **program);
+	puts("END NAMED PARAMETER");
+}
+
+Statement *parse(char **program)
+{
+	puts("------START RECURSION------");
+	Statement *stat = malloc(sizeof(Statement));
+
+	char *c;
+	int params = 0;
+	int expects = -1;
+	bool operfound = false;
+	char slice[opermax + 1];
+	for (c = *program; *c != '\00' && *c != ')'; c++) {
+		strncpy(slice, c, opermax);
+		slice[opermax] = '\00';
+
+		/* Full size operations */
+		/* TODO: Add support for shorthands (& / | / !) */
+		if (strcmp("AND", slice) == 0) {
+			expects = 2;
+
+			if (params != 1) {
+				ERR("Expected one operand preceeding AND");
+			} else if (operfound) {
+				ERR("Expected only a single operation");
+			}
+
+			puts("====[AND]====");
+			stat->op = AND;
+
+			c += 3;
+			operfound = true;
+			continue;
+		} else if (strcmp("NOT", slice) == 0) {
+			expects = 1;
+
+			if (params > 0) {
+				ERR("NOT expects single operand to follow, not preceed");
+			} else if (operfound) {
+				ERR("Expected only a single operation");
+			}
+
+			puts("====[NOT]====");
+			stat->op = NOT;
+
+			c += 3;
+			operfound = true;
+			continue;
+		} else if (strncmp("OR", slice, 2) == 0) {
+			expects = 2;
+
+			if (params != 1) {
+				ERR("Expected one operand preceeding OR");
+			} else if (operfound) {
+				ERR("Expected only a single operation");
+			}
+
+			puts("====[OR]====");
+			stat->op = OR;
+
+			c += 2;
+			operfound = true;
+			continue;
+		}
+
+		if (isspace(*c)) {
+			continue;
+		} else if (*c == '(') {
+			if (expects != -1 && params > expects) {
+				ERR("Too many parameters provided");
+			}
+
+			puts("START STATEMENT PARAMETER");
+			stat->operands[params].t = StatementOperand;
+			c++;
+			stat->operands[params].data.s = (struct Statement *)parse(&c);
+			params++;
+			puts("END STATEMENT PARAMETER");
+		} else {
+			if (expects != -1 && params > expects) {
+				ERR("Too many parameters provided");
+			}
+
+			stat->operands[params].t = NumericOperand;
+			parseparam(&c, stat, params);
+			params++;
+		}
+	}
+
+	if (expects != -1 && params != expects) {
+		ERR("Wrong number of operands provided");
+	} else if (!operfound) {
+		ERR("Statement must contain an operand");
+	}
+
+	puts("------END RECURSION------");
+	printf("Statement ends on '%c'\n", *c);
+
+	*program = c;
+	return stat;
+}
---
+++ truth/truth.h
@@ -0,0 +1,29 @@
+#ifndef TRUTH_H
+#define TRUTH_H
+
+#define ERR(msg)                              \
+	fprintf(stderr, "Error: %s\n", msg);  \
+	exit(1)
+
+typedef enum { AND = 1, OR, NOT } Operation;
+typedef enum {
+	NullOperand = 0,
+	StatementOperand = 1,
+	NumericOperand
+} OperandType;
+typedef struct {
+	OperandType t;
+	union {
+		struct Statement *s;
+		char oper;
+	} data;
+} Operand;
+typedef struct {
+	Operand operands[2];
+	Operation op;
+} Statement;
+
+/* Parsing */
+Statement *parse(char **program);
+
+#endif
