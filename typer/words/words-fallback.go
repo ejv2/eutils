39ec16ab583392a5a6459d6629bddde6d4dcbe18
Ethan Marshall <ethan@ethanjmarshall.co.uk>
2022-01-06 02:01:05 +0000

    Add a few more fallback phrases

--- typer/words/words-fallback.go
+++ typer/words/words-fallback.go
@@ -4,10 +4,12 @@
 //	- A system dictionary could not be found
 package words
 
-var fallbackSentences = [5][]string{
+var fallbackSentences = [7][]string{
 	{"the", "quick", "brown", "fox", "jumped", "over", "the", "lazy", "dog"},
 	{"tell", "be", "about", "the", "rabbits,", "george"},
 	{"stop", "interrupting", "me", "while", "I", "am", "interrupting", "you!"},
 	{"the", "genius", "behind", "the", "genre", "of", "ragtime", "is", "elite", "syncopations"},
 	{"I", "have", "observed", "that", "the", "experience", "of", "a", "programmer", "is", "a", "function", "of", "their", "denisity", "of", "goto", "statements;", "goto", "considered", "harmful"},
+	{"Every", "book", "had", "been,", "rewritten,", "every", "picture", "repainted", "every", "statue", "renamed"},
+	{"The", "industrial", "revolution", "and", "its", "consiquences", "have", "been", "a", "disaster", "for", "the", "human", "race"},
 }
