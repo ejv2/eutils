d113b08dba8bcaa2faa09444597fa1dcc772bcb8
Ethan Marshall <ethan@ethanjmarshall.co.uk>
2022-10-20 11:32:09 +0100

    solve: fix mat_exp call

--- solve/parse.c
+++ solve/parse.c
@@ -33,9 +33,10 @@ mat_t exp_mat(expr_t *exp, size_t n)
 		ret.eval[i] = exp[i].c;
 		j = cur = 0;
 
-		while (j < ret.dims[Unknowns] && cur < exp[i].ncoff) {
+		memset(ret.rows[i], 0, sizeof(long double) * ret.dims[Unknowns]);
+		while (j < ret.dims[Unknowns] && j < exp[i].ncoff) {
 			if ((exp[i].mask & (1 << cur)) > 0) {
-				ret.rows[i][j] = exp[i].coff[cur];
+				ret.rows[i][cur] = exp[i].coff[cur];
 				j++;
 			}
 
