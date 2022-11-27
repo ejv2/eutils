bc00925e2ed910fc935dd82ac0ab9809125823f1
Ethan Marshall <ethan@ethanjmarshall.co.uk>
2022-11-27 13:50:44 +0000

    tally: fix TTY settings

--- tally/tally.c
+++ tally/tally.c
@@ -5,11 +5,15 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 #include <ctype.h>
 #include <getopt.h>
+#include <termios.h>
 
 static const char *flags = "vhuq";
 static int quiet = 0;
+static struct termios termios, termios_orig;
 
 void usage()
 {
@@ -56,32 +60,48 @@ void handle_flags(int argc, char **argv)
 	}
 }
 
-void init_tty()
+void init_tty(int fileno)
 {
-	/* don't line buffer */
-	system("stty -F /dev/tty cbreak min 1");
-	/* don't show output on the TTY */
-	system("stty -F /dev/tty -echo");
+	if (!isatty(fileno) || !isatty(fileno)) {
+		return;
+	}
+	tcgetattr(fileno, &termios_orig);
+	memcpy(&termios, &termios_orig, sizeof(termios_orig));
+
+	termios.c_lflag &= ~ICANON;
+	termios.c_lflag &= ~ECHO;
+
+	tcsetattr(fileno, TCSANOW, &termios);
+}
+
+void reset_tty(int fileno)
+{
+	tcsetattr(fileno, TCSANOW, &termios_orig);
 }
 
 int main(int argc, char **argv)
 {
+	FILE *out;
 	handle_flags(argc, argv);
 
-	init_tty();
+	out = fopen("/dev/tty", "r+");
+	init_tty(fileno(out));
 
 	int t;
-	for (t = 1; getchar() != 'q'; t++) {
+	for (t = 1; getc(out) != 'q'; t++) {
 		if (!quiet) {
 			if (t != 0 && t % 5 == 0) {
-				printf("\r\x1b[9m||||\x1b[0m\n");
+				fprintf(out, "\r\x1b[9m||||\x1b[0m\n");
 				continue;
 			}
 
-			putchar('|');
+			putc('|', out);
 		}
 	}
 
 	t--;
-	printf("\nTotal: %d Gates: %d Extra: %d\n", t, t / 5, t % 5);
+	fprintf(out, "\nTotal: %d Gates: %d Extra: %d\n", t, t / 5, t % 5);
+
+	reset_tty(fileno(out));
+	fclose(out);
 }
